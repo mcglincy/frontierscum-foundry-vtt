5ef6365cd682d1246e87d66ad0b2045bc99bf6e3
mcglincy <mattmcglincy@gmail.com>
2022-07-28 20:12:16 -0500

    Horse sheet.

--- module/actor/sheet/horse-sheet.js
+++ module/actor/sheet/horse-sheet.js
@@ -4,5 +4,21 @@ import FSActorSheet from "./actor-sheet.js";
  * @extends {FSActorSheet}
  */
 export class FSHorseSheet extends FSActorSheet {
-
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["frontierscum", "sheet", "actor", "horse"],
+      template: "systems/frontierscum/templates/actor/horse-sheet.html",
+      width: 700,
+      height: 900,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "personal",
+        },
+      ],
+      dragDrop: [{ dragSelector: ".item-list .item", dropSelector: null }],
+    });
+  }  
 }
